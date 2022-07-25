9f4171ec2ff4f08163a973e0abfc8faad53d7983
EduardoMalkavian <eduardoaraujo33@gmail.com>
2022-07-25 19:56:13 -0300

    Criação de visualização no web de pedido

---
+++ resources/views/Order/create.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cadastrar um novo pedido</title>
+</head>
+
+<body>
+    <form action="">
+        <label for="">Date</label> <br />
+        <input type="text" name="date"> <br />
+        <label for="">Observation</label> <br />
+        <input type="text" name="Observation"> <br />
+        <button>Salvar</button>
+        < </form>
+</body>
+
+</html>
